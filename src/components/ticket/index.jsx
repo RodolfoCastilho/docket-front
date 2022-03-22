80773df9f06391b54dd781811d1a5dbc0bfeb633
rodolfo.castilho <rodolfo.castilho@temmais.com>
2022-03-22 18:44:50 -0300

    adding feedback to delete doc

--- src/components/ticket/index.jsx
+++ src/components/ticket/index.jsx
@@ -1,52 +1,88 @@
 import axios from "axios";
 import "./styles.scss";
+import { useSnackbar } from "react-simple-snackbar";
 
 export const Ticket = ({ data }) => {
+  const errorOpt = {
+    position: "top-center",
+    style: {
+      backgroundColor: "red",
+      color: "white",
+      fontSize: "12px",
+      textAlign: "center",
+    },
+  };
+
+  const sucessOpt = {
+    position: "top-center",
+    style: {
+      backgroundColor: "#00B98E",
+      color: "white",
+      fontSize: "12px",
+      textAlign: "center",
+    },
+  };
+
+  const [openErrorSnackbar] = useSnackbar(errorOpt);
+  const [openSuccessSnackbar] = useSnackbar(sucessOpt);
+
   const formatDate = (date) => {
-    var yyyy = date.substring(0,4);
-    var mm = date.substring(5,7);
-    var dd = date.substring(6,7);
+    var yyyy = date.substring(0, 4);
+    var mm = date.substring(5, 7);
+    var dd = date.substring(6, 7);
 
     return `${dd} do ${mm} de ${yyyy}`;
   };
 
   const handleDeleteItem = async () => {
-    await axios
+    try {
+      await axios
       .delete(`http://localhost:3000/documents/${data.id}`)
-      .then(window.location.reload());
+      .then(function () {
+        window.location.reload();
+        openSuccessSnackbar("Documento excluído com sucesso.");
+      });
+    } catch(e) {
+      openErrorSnackbar("Houve um erro ao excluir o documento.")
+    }
   };
 
   return (
-    <div className="ticket-container">
-      <div className="ticket-header">
-        <h3>{data.documentName}</h3>
-        <button className="delete-button" onClick={handleDeleteItem}></button>
-      </div>
-      <div className="box-data">
-        <div className="private-data">
-          <p>
-            <b>
-              {data.type === "person" ? "Pessoa física" : "Pessoa jurídica"}
-            </b>
-          </p>
-          <p>Nome: {data.name}</p>
-          <p>CPF: {data.documentValue}</p>
+    <>
+      <div className="ticket-container">
+        <div className="ticket-header">
+          <h3>{data.documentName}</h3>
+          <button className="delete-button" onClick={handleDeleteItem}></button>
         </div>
-        <div className="registry-data">
-          <p>
-            <b>Dados do cartório</b>
-          </p>
-          <p>CEP: {data.zipCode}</p>
-          <p>
-            Rua: {data.street} N°: {data.number}
-          </p>
-          <p>
-            Cidade: {data.city} UF: {data.state}
-          </p>
+        <div className="box-data">
+          <div className="private-data">
+            <p>
+              <b>
+                {data.type === "person" ? "Pessoa física" : "Pessoa jurídica"}
+              </b>
+            </p>
+            <p>Nome: {data.name}</p>
+            <p>CPF: {data.documentValue}</p>
+          </div>
+          <div className="registry-data">
+            <p>
+              <b>Dados do cartório</b>
+            </p>
+            <p>CEP: {data.zipCode}</p>
+            <p>
+              Rua: {data.street} N°: {data.number}
+            </p>
+            <p>
+              Cidade: {data.city} UF: {data.state}
+            </p>
+          </div>
         </div>
+        <hr />
+        <p className="ticket-footer">
+          Data de criação: {formatDate(data.createdAt)}
+        </p>
       </div>
-      <hr />
-      <p className="ticket-footer">Data de criação: {formatDate(data.createdAt)}</p>
-    </div>
+
+    </>
   );
 };
